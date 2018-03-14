c0cc5382ffcf9094804bf7bcd28bc7c2f29a762f
Aaron Ho <aaronstevenson.ho@gmail.com>
2018-03-14 13:28:17 +0800

    Trial Commit on Visual Studio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MainPage } from '../main/main';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	mainPage = mainPage;
 
-  }
+  	constructor(public navCtrl: NavController) {
+  	}	
 
+  // pushPage() {
+  // 	this.navCtrl.push(MainPage);
+  // }
 }
